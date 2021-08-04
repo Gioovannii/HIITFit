6f0396c8326f7454ff1c271752d7cccdcb9b4985
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-08-04 17:18:28 +0200

    set conditional statement for url

--- HIITFit/Views/ExerciseView.swift
+++ HIITFit/Views/ExerciseView.swift
@@ -31,17 +31,22 @@
 /// THE SOFTWARE.
 
 import SwiftUI
+import AVKit
 
 struct ExerciseView: View {
     let videoNames = ["squat", "step-up", "burpee", "sun-salute"]
     let exerciseNames = ["Squat", "Step up", "Burpee", "Sun Salute"]
     
     let index: Int
-    
     var body: some View {
         VStack {
             HeaderView(exerciseName: exerciseNames[index])
-            Text("Video player")
+            if let url = Bundle.main.url(forResource: videoNames[index], withExtension: "mp4") {
+                VideoPlayer(player: AVPlayer(url: url))
+            } else {
+                Text("Couldn't find \(videoNames[index]).mp4")
+                    .foregroundColor(.red)
+            }
             Text("Timer")
             Text("Start/Done button")
             Text("Rating")
