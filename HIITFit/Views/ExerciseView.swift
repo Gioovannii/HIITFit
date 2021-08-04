1026f0d5a4b572d34f94d073e4cd7b2fa28146fe
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-08-04 16:11:40 +0200

    extract method

--- HIITFit/Views/ExerciseView.swift
+++ HIITFit/Views/ExerciseView.swift
@@ -40,7 +40,7 @@ struct ExerciseView: View {
     
     var body: some View {
         VStack {
-            Text(exerciseNames[index])
+            HeaderView(exerciseName: exerciseNames[index])
             Text("Video player")
             Text("Timer")
             Text("Start/Done button")
@@ -54,5 +54,23 @@ struct ExerciseView: View {
 struct ExerciseView_Previews: PreviewProvider {
     static var previews: some View {
         ExerciseView(index: 0)
+    }
+}
+
+struct HeaderView: View {
+    let exerciseName: String
+    var body: some View {
+        VStack {
+            Text(exerciseName)
+                .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+            
+            HStack {
+                Image(systemName: "1.circle")
+                Image(systemName: "2.circle")
+                Image(systemName: "3.circle")
+                Image(systemName: "4.circle")
+            }
+            .font(.title2)
+        }
     }
 }
