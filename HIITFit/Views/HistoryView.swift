dc277a7b6327262f3ee267caf11df510683d4c26
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-08-05 13:11:52 +0200

    set some constant for the history view

--- HIITFit/Views/HistoryView.swift
+++ HIITFit/Views/HistoryView.swift
@@ -34,7 +34,15 @@ import SwiftUI
 
 struct HistoryView: View {
     
-  
+    let today = Date()
+    let yesterday = Date().addingTimeInterval(-86400)
+    
+    let exercises1 = ["Squat", "Step Up", "Burpee", "Sun Salute"]
+    let exercises2 = ["Squat", "Step Up", "Burpee"]
+    
+    
+    var body: some View {
+        
     }
 }
 
