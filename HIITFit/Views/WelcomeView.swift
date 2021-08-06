e3aeb7c23930729ba693bd078ee381dfcc4fd64a
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-08-06 17:06:26 +0200

    réarrange views

--- HIITFit/Views/WelcomeView.swift
+++ HIITFit/Views/WelcomeView.swift
@@ -35,22 +35,32 @@ import SwiftUI
 struct WelcomeView: View {
     var body: some View {
         ZStack {
-            HStack(alignment: .bottom) {
-                VStack(alignment: .leading) {
-                    Text("Get fit")
-                        .font(.largeTitle)
-                    Text("with high intensity interval training")
-                        .font(.headline)
-                }
-                Image("step-up")
-                    .resizedToFill(width: 240.0, height: 240.0)
-                    
-            }
             VStack {
                 HeaderView(titleText: "Welcome")
                 Spacer()
                 Button("History") {}
                     .padding(.bottom)
+            }
+            
+            VStack {
+                HStack(alignment: .bottom) {
+                    VStack(alignment: .leading) {
+                        Text("Get fit")
+                            .font(.largeTitle)
+                        Text("with high intensity interval training")
+                            .font(.headline)
+                    }
+                        Image("step-up")
+                            .resizedToFill(width: 240.0, height: 240.0)
+                            .clipShape(Circle())
+                }
+                    Button(action: { }) {
+                        Text("Get started")
+                        Image(systemName: "arrow.right.circle")
+                }
+                .font(.title2)
+                .padding()
+                
             }
         }
     }
