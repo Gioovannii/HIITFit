c0c99d8c8eeabce9d1116163e89eed920f7b8ebd
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-08-09 17:39:08 +0200

    Complete second day

--- HIITFit/Model/HistoryStore.swift
+++ HIITFit/Model/HistoryStore.swift
@@ -51,6 +51,14 @@ extension HistoryStore {
                     Date().addingTimeInterval(-86400),
                         exercises: [
                             Exercise.exercises[0].exerciseName,
-                        ])]
+                            Exercise.exercises[1].exerciseName,
+                            Exercise.exercises[2].exerciseName
+                        ]),
+            ExerciseDay(
+                date:
+                    Date().addingTimeInterval(-86400 * 2),
+                
+                )
+        ]
     }
 }
