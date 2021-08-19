b89b58673db9b020833a50b4fa6dede8dd716c91
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-08-19 18:46:08 +0200

    add property time remaining

--- HIITFit/Views/TimerView.swift
+++ HIITFit/Views/TimerView.swift
@@ -33,6 +33,9 @@
 import SwiftUI
 
 struct TimerView: View {
+    
+    @State private var timeRemaining = 3 // 1
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
