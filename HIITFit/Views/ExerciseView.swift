388be8a54a83294e9d1adddce1a811197707ad94
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-08-27 22:35:55 +0200

    remove  condition statement

--- HIITFit/Views/ExerciseView.swift
+++ HIITFit/Views/ExerciseView.swift
@@ -58,9 +58,6 @@ struct ExerciseView: View {
                     Text("Couldn't find \(Exercise.exercises[index].videoName).mp4")
                         .foregroundColor(.red)
                 }
-                if showTimer {
-                    TimerView(timerDone: $timerDone)
-                }
                 
                 HStack(spacing: 150) {
                     Button("Start exercice") {
