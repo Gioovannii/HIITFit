19a1b5bf7c0fc7891f943cfe71968f4f58367432
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-08-27 22:27:54 +0200

    Add spacer to stick to bottom historique

--- HIITFit/Views/ExerciseView.swift
+++ HIITFit/Views/ExerciseView.swift
@@ -88,6 +88,8 @@ struct ExerciseView: View {
                 RatingView(rating: $rating)
                     .padding()
                 
+                Spacer()
+                
                 Button(NSLocalizedString("History", comment: "view user activity")) {
                     showHistory.toggle()
                 }
